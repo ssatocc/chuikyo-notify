115e0fdb3a89bb822f3d5ddba0d8f5d636e6bbe5
ssatocc <ssatocc@gmail.com>
2024-02-20 02:39:26 +0900

    Update run.py

--- src/run.py
+++ src/run.py
@@ -12,7 +12,8 @@ def update_latest_num(num):
 def notify(num, event_date, agenda_list, docs):
     access_token = os.getenv("ACCESS_TOKEN")
     headers = {"Authorization": f"Bearer {access_token}"}
-    data = {"message": "Hello, World!"}
+    message = f"回数:\n{num}"
+    data = {"message": message}
     requests.post(
         "https://notify-api.line.me/api/notify", headers=headers, data=data
     )
