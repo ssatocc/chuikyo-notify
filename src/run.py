3570cbe44102b8269f6e6426e3a8231a7f553105
ssatocc <ssatocc@gmail.com>
2024-02-19 23:26:21 +0900

    Update src/run.py

--- src/run.py
+++ src/run.py
@@ -3,11 +3,18 @@ from bs4 import BeautifulSoup
 
 
 def main():
-    url = "https://example.com"
+    url = "https://www.mhlw.go.jp/stf/shingi/shingi-chuo_128154.html"
     resp = requests.get(url)
-    soup = BeautifulSoup(resp.text, "html.parser")
+    soup = BeautifulSoup(resp.content, "html.parser")
+
     title = soup.find("title").text
     print(title)
+
+    table = soup.find("table", {"class": "m-tableFlex"})
+    rows = table.findAll("tr")
+    headers, recent = rows[0], rows[1]
+    print(headers)
+    print(recent)
 
 
 if __name__ == "__main__":
