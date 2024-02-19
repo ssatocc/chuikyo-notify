3af7cb965ccb703ca70d7b5067af6825a0a6d7a5
ssatocc <ssatocc@gmail.com>
2024-02-20 00:37:14 +0900

    Update src/run.py

--- src/run.py
+++ src/run.py
@@ -2,23 +2,30 @@ import requests
 from bs4 import BeautifulSoup
 
 
+def validate_headers(headers):
+    ths = headers.findAll("th")
+    th_set = {th.text for th in ths}
+    expected_th_set = {"回数", "資料等", "開催案内", "議題等", "議事録／議事要旨", "開催日"}
+    assert expected_th_set == th_set
+
+
+def validate_title(title):
+    expected_title = "中央社会保険医療協議会(中央社会保険医療協議会総会) ｜厚生労働省"
+    assert expected_title == title
+
+
 def main():
     url = "https://www.mhlw.go.jp/stf/shingi/shingi-chuo_128154.html"
     resp = requests.get(url)
     soup = BeautifulSoup(resp.content, "html.parser")
 
     title = soup.find("title").text
-    expected_title = "中央社会保険医療協議会(中央社会保険医療協議会総会) ｜厚生労働省"
-    assert expected_title == title
+    validate_title(title)
 
     table = soup.find("table", {"class": "m-tableFlex"})
     rows = table.findAll("tr")
     headers, recent = rows[0], rows[1]
-
-    ths = headers.findAll("th")
-    th_set = {th.text for th in ths}
-    expected_th_set = {"回数", "資料等", "開催案内", "議題等", "議事録／議事要旨", "開催日"}
-    assert expected_th_set == th_set
+    validate_headers(headers)
 
     print(recent)
 
