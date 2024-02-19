cd2354fde6875e286d5118e240cbea73a5dd5e14
ssatocc <ssatocc@gmail.com>
2024-02-19 23:43:00 +0900

    Update src/run.py

--- src/run.py
+++ src/run.py
@@ -8,12 +8,18 @@ def main():
     soup = BeautifulSoup(resp.content, "html.parser")
 
     title = soup.find("title").text
-    print(title)
+    expected_title = "中央社会保険医療協議会(中央社会保険医療協議会総会) ｜厚生労働省"
+    assert expected_title == title
 
     table = soup.find("table", {"class": "m-tableFlex"})
     rows = table.findAll("tr")
     headers, recent = rows[0], rows[1]
-    print(headers)
+
+    ths = headers.findAll("th")
+    th_set = {th.text for th in ths}
+    expected_th_set = {"回数", "資料等", "開催案内", "議題等", "議事録／議事要旨", "開催日"}
+    assert expected_th_set == th_set
+
     print(recent)
 
 
